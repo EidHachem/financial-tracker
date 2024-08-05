3a8d55c3246da2ca2194ec960bc3e0581366193d
eid <eidhachem1@gmail.com>
2024-08-05 11:07:14 +0300

    Make expense item component reusable

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const { date, title, price } = props;
+
   return (
     <div className="expense-item">
-      <div>August 5th 2024</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$300.32</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {price}</div>
       </div>
     </div>
   );
